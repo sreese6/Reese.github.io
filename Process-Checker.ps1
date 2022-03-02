d62465509d4f290e1a6104b7379f6deb7605881e
sreese6 <47835406+sreese6@users.noreply.github.com>
2022-03-02 13:02:11 -0500

    Add files via upload

---
+++ Process-Checker.ps1
@@ -0,0 +1,12 @@
+﻿Get-ComputerInfo | Out-File -Append -FilePath .\Process.txt
+Get-WmiObject -Class Win32_Process | Out-File -Append -FilePath .\Process.txt
+Get-WmiObject -Class Win32_Bios | Format-List -Property * | Out-File -Append -FilePath .\Process.txt
+Get-Service | Where-Object {$_.Status -eq "Running"} | Out-File -Append -FilePath .\Process.txt
+Get-EventLog -List | Out-File -Append -FilePath .\Process.txt
+Get-EventLog -LogName System | Out-File -Append -FilePath .\Process.txt
+Get-ComputerInfo | Out-File -Append -FilePath .\Process.txt
+Get-WmiObject -Class Win32_Process | Out-File -Append -FilePath .\Process.txt
+Get-WmiObject -Class Win32_Bios | Format-List -Property * | Out-File -Append -FilePath .\Process.txt
+Get-Service | Where-Object {$_.Status -eq "Running"} | Out-File -Append -FilePath .\Process.txt
+Get-EventLog -List | Out-File -Append -FilePath .\Process.txt
+Get-EventLog -LogName System | Out-File -Append -FilePath .\Process.txt
